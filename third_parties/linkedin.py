dbdf137b0798c01da9748987804d6dc1d9903b49
Richard Strauss <rstrauss91@gmail.com>
2024-02-22 20:09:43 -0500

    fullstack working app

--- third_parties/linkedin.py
+++ third_parties/linkedin.py
@@ -5,11 +5,11 @@ import requests
 def scrape_linkedin_profile(linkedin_profile_url: str):
     """scrape information from LinkedIn profiles,
     Manually scrape the information from the LinkedIn profile"""
-    api_endpoint = "https://nubela.co/proxycurl/api/v2/linkedin"
-    header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
+    api_endpoint = "https://gist.githubusercontent.com/agaveazul/72ac1225ba9a65668cbdb7d5400656d9/raw/737ee54234b57fbec0d6055b729a7e6a3e818b29/richard-strauss.json"
+    # header_dic = {"Authorization": f'Bearer {os.environ.get("PROXYCURL_API_KEY")}'}
 
     response = requests.get(
-        api_endpoint, params={"url": linkedin_profile_url}, headers=header_dic
+        api_endpoint, params={"url": linkedin_profile_url}
     )
 
     data = response.json()
